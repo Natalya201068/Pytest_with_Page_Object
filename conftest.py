1e904d5b012e56d88e331663fa228a04b799ef31
Natalya201068 <dimedroll2010@gmail.com>
2026-05-05 19:09:00 +0300

    added first test

--- conftest.py
+++ conftest.py
@@ -14,7 +14,7 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-    my_proxy = os.getenv("MY_PROXY")
+    my_proxy = os.getenv("PROXY")
     options = Options()
     options.add_argument(f"--proxy-server={my_proxy}")
     options.add_experimental_option('prefs', {
---
+++ pages/main_page.py
@@ -0,0 +1,12 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = WebDriverWait(self.browser, 30).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#login_link')))
+        login_link.click()
+
